b5f104396c118da6087fcf32a8842c394c32e457
king-bob596 <claytonn381@my.clevelandcc.edu>
2024-12-06 10:23:54 -0500

    Add files via upload

---
+++ java lab all together/script.js
@@ -0,0 +1,12 @@
+function changeimage(x,image)
+{
+    if(x==1)
+    {
+image.src= "https://res.cloudinary.com/dtfzqeltb/image/upload/v1731678663/samples/animals/reindeer.jpg";
+    }
+    if(x==2)
+    {image.src="https://res.cloudinary.com/dtfzqeltb/image/upload/v1731678663/samples/food/fish-vegetables.jpg";
+
+    }
+   
+}
